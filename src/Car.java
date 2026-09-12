44d861d4d4dd4e4b4ad563f31d562c0c8fc6540b
Aldibob <axtanobaldiyar@gmail.com>
2026-09-12 17:35:25 +0500

    First commit and creation of first class

---
+++ src/Car.java
@@ -0,0 +1,48 @@
+public class Car {
+
+    private int seats;
+    private int wheels;
+    private String engine;
+    private boolean hasGPS;
+
+    public Car(int seats, int wheels, String engine, boolean hasGPS) {
+        this.seats = seats;
+        this.wheels = wheels;
+        this.engine = engine;
+        this.hasGPS = hasGPS;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public void setHasGps(boolean hasGPS) {
+        this.hasGPS = hasGPS;
+    }
+
+    public void setHasGPS(boolean hasGPS) {
+        this.hasGPS = hasGPS;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+
+
+
+
+
+}
