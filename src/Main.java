6505ff39ba27f237a6622485e03b6af287918936
Aldibob <axtanobaldiyar@gmail.com>
2026-09-16 14:49:28 +0500

    test of SportCar

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         CarDirector director = new CarDirector(); // created director
-        Car car = director.makeDailyCar(new DailyCar()); // Building Daily car
+        Car car = director.makeDailyCar(new DailyCar());// Building Daily car
+
+        CarDirector sport_car_director = new CarDirector();
+        Car sport_car = director.makeSportCar(new SportCar());
 
         System.out.print(car.getWheels());
-        System.out.print(car.isHasGPS());
+        System.out.print(car.isHasGPS() + "\n");
+        System.out.println(sport_car.getEngine());
+        System.out.println(sport_car.getWheels());
     }
 }
